8176727599e120c32fd407eb9ae60a6049d106e6
Gergo Szalai <szerg97@gmail.com>
2023-04-26 15:05:06 +0200

    added more seeds

--- src/main/java/com/szalai/packagetracker/PackageTrackerApplication.java
+++ src/main/java/com/szalai/packagetracker/PackageTrackerApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -32,19 +33,30 @@ public class PackageTrackerApplication {
     @PostConstruct
     public void seed(){
         if (shopRepository.findAll().isEmpty()){
-            Customer customerA = new Customer(UUID.randomUUID().toString(), "Gergo Szalai");
-            Package packA = new Package(UUID.randomUUID().toString(), 24.0, customerA);
+            List<Customer> customers = List.of(
+                    new Customer(UUID.randomUUID().toString(), "Gergo"),
+                    new Customer(UUID.randomUUID().toString(), "Kowalsky"),
+                    new Customer(UUID.randomUUID().toString(), "Schmidt"),
+                    new Customer(UUID.randomUUID().toString(), "Helena"),
+                    new Customer(UUID.randomUUID().toString(), "Peter"));
 
             Shop adidasShop = new Shop("Adidas Oakland", "1022 Oakland, 102. street 99.");
             DistributionPoint distributionPoint = new DistributionPoint("DP New York", "1028 NY, 15. street 1024.");
             Warehouse warehouse = new Warehouse("WH Munich", "90078 MUN, X. Y. street 66.");
-            Destination destination = new Destination("Gergo's House","1088 Budapest, Y. Z. street 33.");
+            List<Destination> destinations = List.of(
+                    new Destination("Gergo's House","1088 Budapest, Y. Z. street 33."),
+                    new Destination("Kowalsky's House","8000 Warsaw, Y. Z. street 43."),
+                    new Destination("Schmidt's House","7865 Vienna, Y. Z. street 22."),
+                    new Destination("Helena's House","6646433 Berlin, Y. Z. street 65."),
+                    new Destination("Peter's House","5345 Győr, Y. Z. street 98."));
 
-            service.post(adidasShop, distributionPoint, LocalDateTime.of(2023, 4, 20, 8, 0), packA.getId());
-            service.distribute(distributionPoint, warehouse, LocalDateTime.of(2023, 4, 21, 8, 0), packA.getId());
-            service.forward(warehouse, destination, LocalDateTime.of(2023, 4, 22, 8, 0), packA.getId());
-            service.arrive(warehouse, destination, LocalDateTime.of(2023, 4, 23, 8, 0), packA.getId());
-
+            for (int i = 0; i < 5; i++) {
+                Package pack = new Package(UUID.randomUUID().toString(), 24.0, customers.get(i));
+                service.post(adidasShop, distributionPoint, LocalDateTime.of(2023, 4, 20, 8, 0), pack.getId());
+                service.distribute(distributionPoint, warehouse, LocalDateTime.of(2023, 4, 21, 8, 0), pack.getId());
+                service.forward(warehouse, destinations.get(i), LocalDateTime.of(2023, 4, 22, 8, 0), pack.getId());
+                service.arrive(warehouse, destinations.get(i), LocalDateTime.of(2023, 4, 23, 8, 0), pack.getId());
+            }
         }
     }
 }
